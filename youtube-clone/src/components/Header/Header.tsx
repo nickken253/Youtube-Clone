7cbae308090a95e9e102284a9ed90d44933d7c9a
nickken2532 <hoanganh.nickken@gmail.com>
2024-05-05 14:28:24 +0700

    add css

--- youtube-clone/src/components/Header/Header.tsx
+++ youtube-clone/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { faBars, faBell, faVideo } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faBell, faMagnifyingGlass, faUser, faVideo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { logo } from '../../assets'
 import React from 'react'
@@ -6,21 +6,20 @@ import React from 'react'
 export const Header = () => {
   // Header gồm có, thanh menu, logo, search bar, icon create new video, icon notification, icon user
   return (
-    <div>
+    <div className='mb-3 mx-5 fixed w-full bg-white z-10 top-0 py-3'>
       <div className="header flex w-full justify-between">
-        <div className='flex justify-center items-center'>
-
-          <button><FontAwesomeIcon icon={faBars} /></button>
-          <img className='h-[16px]' src={logo} alt="Logo" />
+        <div className='flex justify-center items-center ml-5 cursor-pointer'>
+          <button><FontAwesomeIcon icon={faBars} size='xl'/></button>
+          <img className='h-[24px] mx-5' src={logo} alt="Logo" />
         </div>
-        <div className='flex justify-center items-center'>
-          <input type="text" placeholder="Search" />
-          <button>Search</button>
+        <div className='flex justify-between items-center rounded-full w-[36rem] h-fit py-3 px-5 bg-white border border-solid border-[#000000]'>
+          <input type="text" placeholder="Tìm kiếm" className='w-full mx-3 decoration-none outline-none text-black bg-white'/>
+          <button className=''><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#000000",}} /></button>
         </div>
-        <div className='flex justify-center items-center'>
-          <button><FontAwesomeIcon icon={faVideo} /></button>
-          <button><FontAwesomeIcon icon={faBell} /></button>
-          <div className="icon-user">Icon User</div>
+        <div className='flex justify-center items-center gap-5 mx-10'>
+          <button title='Create Video'><FontAwesomeIcon icon={faVideo} size='2xl'/></button>
+          <button title='Alert'><FontAwesomeIcon icon={faBell} size='2xl'/></button>
+          <div className="icon-user cursor-pointer" title='Account Settings'><FontAwesomeIcon icon={faUser} size='2xl'/></div>
         </div>
       </div>
     </div>
--- youtube-clone/src/components/VideoCard/VideoCard.tsx
+++ youtube-clone/src/components/VideoCard/VideoCard.tsx
@@ -13,19 +13,19 @@ interface VideoCardProps {
 export const VideoCard = (props: VideoCardProps) => {
   const { thumbnail, title, channel, views, timestamp, duration, avatar } = props
   return (
-    <div>
+    <div className='a hover:shadow-2xl rounded-2xl p-4 cursor-pointer transition-all ease-in-out duration-250'>
       <div className='aspect-video relative w-full'>
         <img className='object-cover h-full w-full rounded-xl' src={thumbnail} alt="Thumbnail" />
-        <div>{duration}</div>
+        <div className='absolute top-[9rem] right-4 bg-[#00000083] text-white p-1 font-bold rounded-md'>{duration}</div>
       </div>
-      <div className='flex'>
+      <div className='flex mt-5'>
         <div className='w-10 rounded-full'>
           <img className='rounded-full' src={avatar} alt="Avatar" />
         </div>
-        <div>
-          <div>{title}</div>
-          <div>{channel}</div>
-          <div>{views} • {timestamp}</div>
+        <div className='pl-4'>
+          <div className='font-bold'>{title}</div>
+          <div className='text-sm'>{channel}</div>
+          <div className='text-sm'>{views} • {timestamp}</div>
         </div>
       </div>
     </div>
